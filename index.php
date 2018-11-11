9669a7700d01b691d646c8a8b9d27a469952b6c3
Anna Bała <aniabala92@gmail.com>
2018-11-11 21:09:23 +0100

    starting new project simple setup

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Register for the race</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/simple-grid/grid/simple-grid.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <section class="section-intro">
+            <div class="container">
+                <h1>Hello!</h1>
+                <h2>Register today!</h2>
+            </div>
+        </section>
+        <section class="section-registration-form">
+            <div class="container">
+                <form action="" class="form">
+                    <div class="row">
+                        <div class="col-12">
+
+                        </div>
+                    </div>
+                    <label for="name">Imię</label>
+                    <input type="text" placeholder="Imię">
+                </form>
+            </div>
+        </section>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <script src="" async defer></script>
+    </body>
+</html>
